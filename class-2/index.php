54ff8b2caa5258914393066ac12f288c15a8ae1f
Md-Mofakhkharul-Islam-Maruf <159629017+Md-Mofakhkharul-Islam-Maruf@users.noreply.github.com>
2024-03-02 12:08:53 +0600

    Minimize a class in index.php

--- class-2/index.php
+++ class-2/index.php
@@ -30,18 +30,6 @@ $Object_name = new actor();
 $Object_name->Actname = "Sahruk Khan";
 $Object_name->Actmovie = "Pathan";
 echo "Actor name is: ".$Object_name->Actname."<br>Movie name is: ".$Object_name->Actmovie."<br>";
-
-
-class person {
-    public $First_person;
-    public $Second_person;
-}
-$obj = new person;
-$obj->First_person = "Maruf";
-$obj->Second_person = "Hasan";
-echo "First person name is: ". $obj->First_person."<br>";
-echo "Second person name is: ". $obj->Second_person. "<br>";
-var_dump($obj->First_person);
 ?>
 </body>
 </html>
