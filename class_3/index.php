6a979af9f4740639111a4b76dc3269d35238b7db
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2024-03-02 16:45:31 +0600

    day 2 1/2 done

--- class_3/index.php
+++ class_3/index.php
@@ -3,11 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Class 3</title>
+    <title>Variable scope</title>
 </head>
 <body>
     <?php
-    echo "Variable practice";
-    ?>
+    $Bangladesh = 1971;   // global scope
+    function age()
+    {
+        $Pakisthan = 1900;       //Local scope
+        echo "I Love Bangladesh";
+    }
+    echo age()."<br>";
+    echo $Bangladesh."<br>";
+
+    $Num1 = 4;
+    $Num2 = 3;
+    function check_scope()
+    {
+        global $Num1, $Num2;
+        echo "From Check function <br>";
+        echo $num3 = $Num1 + $Num2;
+    }
+    echo check_scope()."<br>";
+    define("Sitename", "My Portfolio Site <br>");
+    echo Sitename;
+    define ('PI',"3.1416");
+    echo PI;
+?>
 </body>
 </html>
