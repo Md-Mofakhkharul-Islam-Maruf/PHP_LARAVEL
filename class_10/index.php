90761de319df474942b34b01d320510ef1e979b7
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2024-03-13 14:44:43 +0600

    m

---
+++ class_10/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <header class="container0">
+        <div class="row">
+            <div class="col-md-6 order-md-1 col-lg-3 order-lg-1 col-xl-4 order-xl-1 border border-2">
+                <div class="bg-danger"><h2>Logo Here</h2></div>
+            </div>
+        </div>    
+</body>
+</html>
