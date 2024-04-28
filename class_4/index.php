33011e631b9ab73bb01c93f13e0046a77950d42c
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2024-04-28 09:54:16 +0600

    class 5 done

--- class_4/index.php
+++ class_4/index.php
@@ -1,37 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>full HTML and php</title>
-    <link rel= "icon" type="image/x-icon" href="html.jpg">
+    <link rel="icon" type="image/x-icon" href="html.jpg">
 </head>
+
 <body>
-    <h1 text-align= "center">HTML</h1>
+    <h1 text-align="center">HTML</h1>
     <h2>HTML</h2>
     <h4>HTML</h3>
-    <h5>HTML</h5>
-    <h6>HTML</h6>
-    <img src="map.jpg" usemap="#image-map">
+        <h5>HTML</h5>
+        <h6>HTML</h6>
+        <img src="map.jpg" usemap="#image-map">
 
-<map name="image-map">
-    <area target="_blank" alt="Lalmonirhat" title="Lalmonirhat" href="https://www.lalmonirhat.gov.bd/" coords="204,228,197,223,214,238,214,254,220,260,227,266,238,275,246,280,257,285,264,282,266,274,258,255,237,245,226,235,217,220,214,208,206,200,195,197,194,209,195,211" shape="poly">
-</map>
-    <p>
-    My country’s name is <strong>Bangladesh.</strong> It is situated in eastern <em>South Asia</em> and is bordered by the <mark>Bay of Bengal</mark> in the south, India, and Myanmar to the east, and just India to the west. Its general area is <del>147,570</del> <ins>148,460</ins> square kilometers, including waterways. It achieved <small>independence</small> after a brutal liberation fight. <blockquote>Approximately three million men and women gave their lives for the cause of independence. </blockquote>  After attaining independence, <sup>the newborn nation</sup> went through many difficult times and <sub>challenges</sub>. However, it rises nicely. <q>Bangladesh is now one of the greatest places to live in South Asia. </q> There are so many beautiful places to see in our nation.</p>
-    <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, illum.</p> -->
-    <address>
-        <ul>
-            <li>
-                <span>Location:</span> <a href="https://maps.app.goo.gl/Ei5rbuQVw9CoSeru6" target="blank"> Banasree, Rampura, Dhaka </a><br>
-            </li>
-            <li>
-                <span>Contact: </span> <a href="tel:+880179798567"target="blank" > +880179798567</a><br>
-            </li>
-            <li>
-                <span>Email:</span>  <a href=" mailto: mofakhkharulislammaruf@gmail.com" target="blank">mofakhkharulislammaruf@gmail.com</a><br>
-            </li>   
+        <map name="image-map">
+            <area target="_blank" alt="Lalmonirhat" title="Lalmonirhat" href="https://www.lalmonirhat.gov.bd/" coords="204,228,197,223,214,238,214,254,220,260,227,266,238,275,246,280,257,285,264,282,266,274,258,255,237,245,226,235,217,220,214,208,206,200,195,197,194,209,195,211" shape="poly">
+        </map>
+        <p>
+            My country’s name is <strong>Bangladesh.</strong> It is situated in eastern <em>South Asia</em> and is bordered by the <mark>Bay of Bengal</mark> in the south, India, and Myanmar to the east, and just India to the west. Its general area is <del>147,570</del> <ins>148,460</ins> square kilometers, including waterways. It achieved <small>independence</small> after a brutal liberation fight.
+        <blockquote>Approximately three million men and women gave their lives for the cause of independence. </blockquote> After attaining independence, <sup>the newborn nation</sup> went through many difficult times and <sub>challenges</sub>. However, it rises nicely. <q>Bangladesh is now one of the greatest places to live in South Asia. </q> There are so many beautiful places to see in our nation.</p>
+        <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, illum.</p> -->
+        <address>
+            <ul>
+                <li>
+                    <span>Location:</span> <a href="https://maps.app.goo.gl/Ei5rbuQVw9CoSeru6" target="blank"> Banasree, Rampura, Dhaka </a><br>
+                </li>
+                <li>
+                    <span>Contact: </span> <a href="tel:+880179798567" target="blank"> +880179798567</a><br>
+                </li>
+                <li>
+                    <span>Email:</span> <a href=" mailto: mofakhkharulislammaruf@gmail.com" target="blank">mofakhkharulislammaruf@gmail.com</a><br>
+                </li>
+            </ul>
             <!-- validator.w3.org  -->
-    </address>
+        </address>
 </body>
+
 </html>
